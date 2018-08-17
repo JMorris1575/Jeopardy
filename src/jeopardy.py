cb6885d53ac4f536bc59df566332fe503e90b73b
JMorris1575 <FrJamesMorris@gmail.com>
2018-08-17 08:12:47 -0400

    Refactoring: Moved menu click handlers to jeopardy_ui.py

--- src/jeopardy.py
+++ src/jeopardy.py
@@ -24,7 +24,7 @@ class Jeopardy(QMainWindow, jeopardy_ui.JeopardyUI):
         self.setProgramMode(ProgramMode.Empty)
         self.game_segment = Segment.Jeopardy
 
-        self.category_font = QFont("Arial", 36)
+        self.category_font = QFont("Arial", 18)
         self.clue_font = QFont("Times New Roman", 18)
         self.number_font = QFont("Arial", 32, QFont.Bold)
 
@@ -36,135 +36,6 @@ class Jeopardy(QMainWindow, jeopardy_ui.JeopardyUI):
         desktop = self.app.desktop()
         screenNumber = desktop.screenNumber(self)  # gets the screen the form is on
         return desktop.availableGeometry(screenNumber)  # as a QRect
-
-    def file_open(self):
-        print("Got to file_open.")
-        print("Opening temporary game file: 'temp_saved_game'")
-        if self.game_modified:
-            print("Write the code to ask if the user wants to save the current game before opening a new one.")
-        self.game = self.game.read_game('temp_saved_game')
-        self.game.playable = True
-        print("The game is marked playable = ", self.game.playable)
-        self.game_loaded = True
-        self.setProgramMode(ProgramMode.Neutral)
-        self.hideCategories(self.game_segment)
-        self.fillBoard(self.game, Segment.Jeopardy)
-
-    def file_create(self):
-        print("Got to file_create.")
-        self.game_loaded = True
-        self.setProgramMode(ProgramMode.Editing)
-        self.board.category_displays[3].display_state = DisplayState.Category
-
-    def file_close(self):
-        print("Got to file_close.")
-        # check for unsaved changes here
-        self.game = Game()                      # clear out all the former game entries
-        self.game_loaded = False
-        self.setProgramMode(ProgramMode.Neutral)
-
-    def file_save(self):
-        print("Got to file_save.")
-
-    def file_save_as(self):
-        print("Got to file_save_as.")
-
-    def file_print(self):
-        print("Got to file_print")
-
-    def file_exit(self):
-        print("Got to file_exit.")
-        self.close()
-
-    def closeEvent(self, event):
-        print("Got to closeEvent.", event)
-        # check for unsaved files here
-
-    def edit_modify_info(self):
-        print("Got to edit_modify.")
-        self.setProgramMode(ProgramMode.Editing)
-
-    def edit_modify_jeopardy(self):
-        print("Got to edit_modify_jeopardy.")
-        self.setProgramMode(ProgramMode.Editing)
-        self.setSegment(Segment.Jeopardy)
-        self.revealCategories()
-
-    def edit_modify_double_jeopardy(self):
-        print("Got to edit_modify_double_jeopardy.")
-        self.setProgramMode(ProgramMode.Editing)
-        self.setSegment(Segment.DoubleJeopardy)
-        self.revealCategories()
-
-    def edit_modify_final_jeopardy(self):
-        print("Got to edit_modify_final_jeopardy.")
-        self.setProgramMode(ProgramMode.Editing)
-        self.setSegment(Segment.FinalJeopardy)
-        self.revealCategories()
-
-    def edit_cut(self):
-        print("Got to edit_cut.")
-
-    def edit_copy(self):
-        print("Got to edit_copy.")
-
-    def edit_paste(self):
-        print("Got to edit_paste.")
-
-    def game_names(self):
-        print("Got to game_names.")
-        self.clue_displays[2][1].display_state = DisplayState.Clue
-
-    def game_practice(self):
-        print("Got to game_practice.")
-
-    def game_play_jeopardy(self):
-        print("Got to game_play_jeopardy.")
-        self.setProgramMode(ProgramMode.Playing)
-        self.setSegment(Segment.Jeopardy)
-        self.hideCategories(Segment.Jeopardy)
-
-    def game_play_double_jeopardy(self):
-        print("Got to game_play_double_jeopardy")
-        self.setProgramMode(ProgramMode.Playing)
-        self.setSegment(Segment.DoubleJeopardy)
-        self.hideCategories(Segment.DoubleJeopardy)
-
-    def game_play_final_jeopardy(self):
-        print("Got to game_play_final_jeoardy")
-        self.setProgramMode(ProgramMode.Playing)
-        self.setSegment(Segment.FinalJeopardy)
-        self.hideCategories(Segment.FinalJeopardy)
-
-    def game_correct(self):
-        print("Got to game_correct.")
-
-    def game_end(self):
-        """
-        Ends the current game and places the program in Neutral mode
-        :return: None
-        """
-        print("Got to game_end")
-        # :todo: First there should be a warning message before ending the game
-        self.setProgramMode(ProgramMode.Neutral)
-
-
-    def game_settings(self):
-        print("Got to game_settings.")
-
-        import time
-        time.sleep(5)
-        print("That was a quick sleep!")
-
-
-    def help_using_program(self):
-        print("Got to help_using_program.")
-
-    def help_rules(self):
-        print("Got to help_rules.")
-
-    def help_about(self):
-        print("Got to help_about.")
 
     def setProgramMode(self, mode):
         self.program_mode = mode
