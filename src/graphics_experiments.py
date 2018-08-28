c09f749ad6c1263ae38f8b50f3eee7f0b541d569
JMorris1575 <FrJamesMorris@gmail.com>
2018-08-27 21:27:52 -0400

    Added Experiments in panning scene sideways

--- src/graphics_experiments.py
+++ src/graphics_experiments.py
@@ -5,27 +5,45 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+import time
+
 class GraphicsTester(QMainWindow):
 
     def __init__(self, parent=None):
         super(GraphicsTester, self).__init__(parent)
 
-        fakeMenu = self.menuBar().addMenu('&Fake Item')
+        testMenu = self.menuBar().addMenu('&Tests')
+        self.motion_action = testMenu.addAction('&Motion')
+        self.motion_action.triggered.connect(self.MoveScene)
 
         self.view = QGraphicsView()
         self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.scene = QGraphicsScene()
-        newPen = QPen()
-        newPen.setColor(Qt.red)
-        newPen.setWidth(5)
+        redPen = QPen()
+        redPen.setColor(Qt.red)
+        redPen.setWidth(5)
         self.rect = QGraphicsRectItem()
-        self.rect.setPen(newPen)
+        self.rect.setPen(redPen)
         self.rect.setRect(0, 0, 300, 200)
+        self.rect02 = QGraphicsRectItem(310, 0, 300, 200)
+        bluePen = QPen()
+        bluePen.setColor(Qt.blue)
+        bluePen.setWidth(5)
+        self.rect02.setPen(bluePen)
         self.scene.addItem(self.rect)
+        self.scene.addItem((self.rect02))
         self.view.setScene(self.scene)
         self.setCentralWidget(self.view)
         self.view.setSceneRect(-10, -10, 320, 220)
+
+    def MoveScene(self):
+        increment = 310/49
+        for i in range(50):
+            self.view.setSceneRect(0 + increment * i, 0, 300, 200)
+            time.sleep(0.0167)
+            self.view.repaint()
+
 
 if __name__ == '__main__':
     import sys
